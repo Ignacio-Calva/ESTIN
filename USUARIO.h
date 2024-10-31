5001bf84b057e7930e2093b35edaffa805b32075
Xhispas <acevedo.candela066@gmail.com>
2024-10-31 01:19:58 -0600

    arreglitos.......

--- USUARIO.h
+++ USUARIO.h
@@ -6,7 +6,8 @@
 class  usuario: public Persona
 {
 private:
-    int ID_Usuario = 0; //iniciado en 0 para que asigne IDs de 1 en adelante
+    int ID_Usuario = 0;
+    //iniciado en 0 para que asigne IDs de 1 en adelante
     /*char NombreUsuario[30] = {};
     char Contrasenia[20] = {};
     int Edad; //agregué nuevas variables
@@ -20,52 +21,16 @@ public:
     void cargarDatos() //expandí los métodos y añadí SETTERS y GETTERS con las nuevas variables
     {
         cargarDatosPersona();
-        /*cout << "INGRESE EL NOMBRE DE USUARIO: ";
-        cargarCadena(NombreUsuario, 29);
-        cout << "INGRESE LA CONTRASENIA DEL USUARIO: ";
-        cargarCadena(Contrasenia, 19);
-        cout<< "INGRESE SU EDAD: ";
-        cin>> Edad;
-        cout<< "INGRESE SU DNI: ";
-        cin>> DNI;
-        cout<< "INGRESE SU MAIL: ";
-        cargarCadena(Mail, 29);
-        cout<< "INGRESE EL ID DE SU PAIS: ";
-        cin>> IDPais;*/
         setAdmin(false);
         activo = true;
     }
 
-    void mostrarDatos()
-    {
-        mostrarDatosPersona();
-        //cout<< "Nombre usuario: " << NombreUsuario << endl;
-        cout<< "ID Usuario: " << ID_Usuario << endl;
-        /*cout<< "Edad: " << Edad << endl;
-        cout<< "DNI:  " << DNI << endl;
-        cout<< "Mail: " << Mail << endl;
-        cout<< "ID de Pais: " << IDPais << endl;*/
-    }
-
+    void mostrarDatos(){mostrarDatosPersona(); cout<< "ID Usuario: " << ID_Usuario << endl;}
     void setID(int id){ID_Usuario=id;}
     void setActivo(bool esActivo){activo = esActivo;}
-    /*void setNombre(const char *n){strcpy(NombreUsuario, n);}
-    void setContrasenia(const char *c){strcpy(Contrasenia, c);}
-    void setEdad(int e){Edad=e;}
-    void setDNI(int d){DNI=d;}
-    void setMail(const char *m){strcpy(Mail, m);}
-    void setIDPais(int p){IDPais=p;}
-    void setAdmin(bool a){admin=a;}*/
-
     int getID (){return ID_Usuario;}
     bool getActivo(){return activo;}
-    /*const char* getNombre(){return NombreUsuario;}
-    const char* getContrasenia(){return Contrasenia;}
-    int getEdad(){return Edad;}
-    int getDNI(){return DNI;}
-    const char* getMail(){return Mail;}
-    int getIDPais(){return IDPais;}
-    bool getAdmin(){return admin;}*/
+
 };
 
 class archivoUsuario
